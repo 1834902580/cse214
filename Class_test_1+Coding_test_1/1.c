666bbfa39b68f23239470d055f0338cae46bd486
Asaduzzaman <65646561+1834902580@users.noreply.github.com>
2020-08-06 16:39:15 +0600

    Add files via upload

---
+++ Class_test_1+Coding_test_1/1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int n, i = 0;
+    int a[100];
+
+    scanf("%d",&n);
+    while(scanf("%d",&a[i])==1){
+    i++;
+    if(i>n){
+        printf("Array Overflow\n");
+        break;
+        }
+    }
+    int j;
+    for(j=0; j<i; j++)
+    {
+        printf("%d ",a[j]);
+    }
+
+    while(scanf("%d",&a[i]) != EOF){
+        i++;
+    }
+    for(j=0; j<i; j++)
+    {
+        printf("%d ",a[j]);
+    }
+
+    while(scanf("%d",&a[i]) == 1){
+        i++;
+        if(a[i] == 0){
+            break;
+        }
+    }
+    for(j=0; j<i; j++)
+    {
+        printf("%d ",a[j]);
+    }
+
+  return 0;
+}
